c96a1c632ba8f1cd54976fb212aab9a4e3a48311
Harsha Javalkar <harshajavalkar28@gmail.com>
2021-06-10 00:41:45 +0530

    third-rebuild commit

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const express = require('express');
-const path = require('path');
+
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/git'));
+app.use(express.static("./dist/git"));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/git/'}),
-);
+app.get("/*", (req, res) => res.sendFile("index.html", { root: "dist/git/" }));
 
-
-
-const cors = require('cors'); 
-const express = require('express');
-const app = express();app.use(cors());
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors());
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
